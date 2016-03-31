71ed233ddf1c72d37fe275cb2d89c364ffdeeda1
xiphodon <827789895@qq.com>
2016-03-31 23:27:43 +0800

    扇形自定义菜单基础功能已实现

---
+++ menu/src/main/java/com/gc/administrator/activity/MainActivity.java
@@ -0,0 +1,126 @@
+package com.gc.administrator.activity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.gc.administrator.animutils.AnimUtils;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView iv_home;
+    //显示二级菜单
+    private boolean isShoewlevel2 = true;
+    //显示三级菜单
+    private boolean isShoewlevel3 = true;
+    //显示所有菜单
+    private boolean isShoewAllMenu = true;
+
+    private RelativeLayout rl_level1,rl_level2,rl_level3;
+    private ImageView iv_menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initUI();
+        initListener();
+    }
+
+    /**
+     * 初始化监听事件
+     */
+    private void initListener() {
+        iv_home.setOnClickListener(this);
+        iv_menu.setOnClickListener(this);
+    }
+
+    /**
+     * 初始化UI
+     */
+    private void initUI() {
+        setContentView(R.layout.activity_main);
+        iv_home = (ImageView) findViewById(R.id.iv_home);
+        iv_menu = (ImageView) findViewById(R.id.iv_menu);
+        rl_level1 = (RelativeLayout) findViewById(R.id.rl_level1);
+        rl_level2 = (RelativeLayout) findViewById(R.id.rl_level2);
+        rl_level3 = (RelativeLayout) findViewById(R.id.rl_level3);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_home:
+                if (isShoewlevel2) {
+                    //点击隐藏
+                    int startOffset = 0;
+                    //二级，三级菜单都显示时，点击R.id.iv_home，三级、二级菜单先后关闭
+                    if(isShoewlevel3){
+                        AnimUtils.closeMenu(rl_level3,startOffset);
+                        startOffset += 150;
+                        isShoewlevel3 = !isShoewlevel3;
+                    }
+                    AnimUtils.closeMenu(rl_level2,startOffset);
+                } else {
+                    //点击显示
+                    AnimUtils.showMenu(rl_level2,0);
+                }
+                isShoewlevel2 = !isShoewlevel2;
+                break;
+
+            case R.id.iv_menu:
+                if (isShoewlevel3) {
+                    //点击隐藏
+                    AnimUtils.closeMenu(rl_level3,0);
+                } else {
+                    //点击显示
+                    AnimUtils.showMenu(rl_level3,0);
+                }
+                isShoewlevel3 = !isShoewlevel3;
+                break;
+        }
+    }
+
+    /**
+     * 物理按键点击监听
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //当点击菜单键
+        if(keyCode == KeyEvent.KEYCODE_MENU){
+            if(isShoewAllMenu){
+                //关闭所有菜单
+                int startOffset = 0;
+                if(isShoewlevel3){
+                    AnimUtils.closeMenu(rl_level3,startOffset);
+                    startOffset += 150;
+                    isShoewlevel3 = !isShoewlevel3;
+                }
+                if(isShoewlevel2){
+                    AnimUtils.closeMenu(rl_level2,startOffset);
+                    startOffset += 150;
+                    isShoewlevel2 = !isShoewlevel2;
+                }
+                AnimUtils.closeMenu(rl_level1,startOffset);
+            }else{
+                //显示所有菜单
+                AnimUtils.showMenu(rl_level1,0);
+                AnimUtils.showMenu(rl_level2,150);
+                isShoewlevel2 = !isShoewlevel2;
+                AnimUtils.showMenu(rl_level3,300);
+                isShoewlevel3 = !isShoewlevel3;
+            }
+            isShoewAllMenu = !isShoewAllMenu;
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ menu/src/main/java/com/gc/administrator/animutils/AnimUtils.java
@@ -0,0 +1,53 @@
+package com.gc.administrator.animutils;
+
+import android.view.animation.RotateAnimation;
+import android.widget.RelativeLayout;
+
+/**
+ * 动画工具类
+ */
+public class AnimUtils {
+    /**
+     * 关闭菜单
+     * @param relativeLayout 相对布局
+     * @param startOffset 动画延时时间
+     */
+    public static void closeMenu(RelativeLayout relativeLayout,long startOffset) {
+        //遍历当前RelativeLayout的所有子View，全部设置为不可用，在关闭菜单后，使原位置点击失效
+        for (int i = 0; i < relativeLayout.getChildCount() ; i++) {
+            relativeLayout.getChildAt(i).setEnabled(false);
+        }
+        //旋转动画，逆时针旋转180度，参照自己宽度的一半比例，高度的全部比例
+        RotateAnimation rotateAnimation = new RotateAnimation(0, -180, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 1.0f);
+        //旋转时间500毫秒
+        rotateAnimation.setDuration(300);
+        //动画旋转后保持最后的状态
+        rotateAnimation.setFillAfter(true);
+        //延时startOffset毫秒后开始动画
+        rotateAnimation.setStartOffset(startOffset);
+        //布局加载动画
+        relativeLayout.startAnimation(rotateAnimation);
+    }
+
+    /**
+     * 显示菜单
+     * @param relativeLayout 相对布局
+     * @param startOffset 动画延时时间
+     */
+    public static void showMenu(RelativeLayout relativeLayout,long startOffset) {
+        //遍历当前RelativeLayout的所有子View，全部设置为可用，在开启菜单后，使原位置点击生效
+        for (int i = 0; i < relativeLayout.getChildCount() ; i++) {
+            relativeLayout.getChildAt(i).setEnabled(true);
+        }
+        //旋转动画，顺时针旋转180度，参照自己宽度的一半比例，高度的全部比例
+        RotateAnimation rotateAnimation = new RotateAnimation(-180, 0, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 1.0f);
+        //旋转时间500毫秒
+        rotateAnimation.setDuration(300);
+        //动画旋转后保持最后的状态
+        rotateAnimation.setFillAfter(true);
+        //延时startOffset毫秒后开始动画
+        rotateAnimation.setStartOffset(startOffset);
+        //布局加载动画
+        relativeLayout.startAnimation(rotateAnimation);
+    }
+}
